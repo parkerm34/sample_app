29d36f55e3ea87937e3988ffb2111dda05ef90af
Parker Mathewson <parkerm34@bio5.org>
2013-04-25 14:53:17 -0700

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,7 +28,14 @@ describe "User pages" do
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
+        
       end
+      describe "after submission" do
+        before { click_button submit }
+          
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end 
     end
 
     describe "with valid information" do
@@ -37,10 +44,20 @@ describe "User pages" do
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
-      end
+        
+        describe "after saving the user" do
+          before { click_button submit }
+          let(:user) { User.find_by_email('user@example.com') }
 
-      it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
+          it { should have_link('Sign out') }
+          it { should have_selector('title', text: user.name) }
+          it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        end
+        
+        it "should create a user" do
+          expect { click_button submit }.to change(User, :count).by(1)
+        end
+        
       end
     end
   end
